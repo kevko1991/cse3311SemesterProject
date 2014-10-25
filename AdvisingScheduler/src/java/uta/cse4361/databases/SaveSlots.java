06049afe279d762abcd811809a56fa6ea6de7b6d
asdfghjqwerty <andrew_t52@hotmail.com>
2014-10-25 11:50:37 -0500

    SaveSlots WIP

--- AdvisingScheduler/src/java/uta/cse4361/databases/SaveSlots.java
+++ AdvisingScheduler/src/java/uta/cse4361/databases/SaveSlots.java
@@ -4,11 +4,42 @@
  * and open the template in the editor.
  */
 package uta.cse4361.databases;
-
+import java.util.*;
+import java.sql.*;
+import uta.cse4361.businessobjects.AvailableSlot;
+import uta.cse4361.businessobjects.Slot;
 /**
  *
  * @author Han
  */
-public class SaveSlots {
+
+public class SaveSlots extends RDBImplCommand {
+    ArrayList<Slot> slots;
+    String sqlQuery = "INSERT INTO SLOT(SlotDate, SlotStartHour, SlotStartMin) VALUES (?, ?, ?)";
     
+    public SaveSlots(ArrayList<Slot> slots){
+        this.slots = slots;
+    }
+    
+    public void queryDB() throws SQLException{
+        try{
+            for(Slot s: slots){
+                statement = conn.prepareStatement(sqlQuery);
+                statement.setDate(1, new java.sql.Date(s.getDate().getTime()));
+                statement.setInt(2, (s.getTime()/100));
+                statement.setInt(3, (s.getTime()%100));
+                statement.executeUpdate();
+                System.out.println("inserted data");
+            }
+        }
+        catch (SQLException e){
+            System.out.println("failed");
+        } finally {
+            if(statement != null){
+                statement.close();
+            }
+        }
+    }
+    public void processResult(){};
+
 }
---
+++ AdvisingScheduler/test/uta/cse4361/databases/SaveSlotsTest.java
@@ -0,0 +1,70 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uta.cse4361.databases;
+import uta.cse4361.businessobjects.Slot;
+import uta.cse4361.businessobjects.AvailableSlot;
+import java.util.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Andrew
+ */
+public class SaveSlotsTest {
+    
+    public SaveSlotsTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of queryDB method, of class SaveSlots.
+     */
+    @Test
+    public void testQueryDB() throws Exception {
+        System.out.println("queryDB");
+        ArrayList<Slot> slots = new ArrayList<Slot>();
+        slots.add(new AvailableSlot(new Date(), 8, 0));
+        slots.add(new AvailableSlot(new Date(), 8, 15));
+        slots.add(new AvailableSlot(new Date(), 8, 30));
+        slots.add(new AvailableSlot(new Date(), 8, 45));
+        slots.add(new AvailableSlot(new Date(), 9, 0));
+        slots.add(new AvailableSlot(new Date(), 9, 15));
+        SaveSlots instance = new SaveSlots(slots);
+        instance.execute();
+    }
+
+    /**
+     * Test of processResult method, of class SaveSlots.
+     */
+    @Test
+    public void testProcessResult() {
+        System.out.println("processResult");
+        SaveSlots instance = new SaveSlots(new ArrayList<Slot>());
+        instance.processResult();
+    }
+    
+}
