b378a2be1d39684de02105a39bd4fca89583a434
Frank Robinson <totallyunique11@gmail.com>
2014-11-20 21:19:52 -0600

    First Mocking test case installed.  Will update project to work with new jars

--- AdvisingScheduler/test/uta/cse4361/databases/GetSlotTest.java
+++ AdvisingScheduler/test/uta/cse4361/databases/GetSlotTest.java
@@ -5,18 +5,37 @@
  */
 package uta.cse4361.databases;
 
+import com.mockrunner.jdbc.BasicJDBCTestCaseAdapter;
+import com.mockrunner.jdbc.PreparedStatementResultSetHandler;
+import com.mockrunner.jdbc.StatementResultSetHandler;
+import com.mockrunner.mock.jdbc.MockConnection;
+import com.mockrunner.mock.jdbc.MockResultSet;
+import java.util.ArrayList;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import uta.cse4361.businessobjects.Slot;
 
 /**
  *
  * @author Andrew
  */
-public class GetSlotTest {
+public class GetSlotTest extends BasicJDBCTestCaseAdapter{
+    
+    private void prepareResultSet(){
+
+        MockConnection connection = getJDBCMockObjectFactory().getMockConnection();
+        PreparedStatementResultSetHandler resultSetHandler = connection.getPreparedStatementResultSetHandler();
+
+        MockResultSet result = resultSetHandler.createResultSet();
+        result.addRow(new String[] {"1", "2014-11-14", "8", "15"});
+
+        resultSetHandler.prepareGlobalResultSet(result);
+    }
+
     
     public GetSlotTest() {
     }
@@ -43,9 +62,12 @@ public class GetSlotTest {
     @Test
     public void testQueryDB() throws Exception {
         System.out.println("queryDB");
+        prepareResultSet();
         GetSlot instance = new GetSlot();
         instance.execute();
         assertNotNull(instance.getResult());
+        ArrayList<Slot> pulledSlots = (ArrayList<Slot>) instance.getResult();
+        assertEquals(8, pulledSlots.get(0).getHour());
     }
     
 }
