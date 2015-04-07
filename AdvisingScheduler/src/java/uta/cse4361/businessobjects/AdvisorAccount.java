cf001f98160674b744885c1af6a4789191a2a805
rwalker2014 <ross.walker@mavs.uta.edu>
2015-04-07 09:58:03 -0500

    Merge branch 'master' into LeadAdvisorManagementPage
    Conflicts:
    	AdvisingScheduler/src/java/uta/cse4361/businessobjects/AdvisorAccount.java
    	AdvisingScheduler/src/java/uta/cse4361/databases/DatabaseImpInterface.java
    	AdvisingScheduler/src/java/uta/cse4361/databases/RelationalDatabaseImpl.java
    	AdvisingScheduler/web/index.jsp
    	AdvisingScheduler/web/navigationbar.jsp

--- AdvisingScheduler/src/java/uta/cse4361/businessobjects/AdvisorAccount.java
+++ AdvisingScheduler/src/java/uta/cse4361/businessobjects/AdvisorAccount.java
@@ -1,151 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package uta.cse4361.businessobjects;
 
-/**
- *
- * @author Andrew
- */
-public class AdvisorAccount {
-    
-    private String name;
-    private String email;
-    private String department;
-    private int ID;
-    private String tempPassword;
-    private int rank;
-    private String assignment;
-    
-    public AdvisorAccount(){
-        
-    }
-    
-    public boolean initialize(String name, String email, String department, String tempPassword, int rank){
-        boolean result = true;
-        
-        if(name != null && !name.isEmpty() && 
-                email != null && !email.isEmpty() && 
-                department != null && !department.isEmpty() && 
-                tempPassword != null && !tempPassword.isEmpty())
-        {
-            this.name = name;
-            this.email = email;
-            this.department = department;
-            this.tempPassword = hashPassword(tempPassword);
-            this.rank = rank;
-        }
-        else
-        {
-            result = false;
-        }
-        
-        return result;
-    }
-    
-    public boolean initialize(String name, String email, String department, int ID, int rank){
-        boolean result = true;
-        
-        if(name != null && !name.isEmpty() && 
-                email != null && !email.isEmpty() && 
-                department != null && !department.isEmpty())
-        {
-            this.name = name;
-            this.email = email;
-            this.department = department;
-            this.rank = rank;
-        }
-        else
-        {
-            result = false;
-        }
-        
-        return result;
-    }
-    
-    public boolean initialize(String name, String department, int ID, String assignment){
-        boolean result = true;
-        
-        if(name != null && !name.isEmpty() && 
-                department != null && !department.isEmpty())
-        {
-            this.name = name;
-            this.department = department;
-            this.ID = ID;
-            this.assignment = assignment;
-        }
-        else
-        {
-            result = false;
-        }
-        
-        return result;
-    }
-    
-    public static String hashPassword(String tempPassword)
-    {
-        int hash = tempPassword.hashCode();
-        return Integer.toString(hash);
-    }
-    
-    public void setName(String name){
-        this.name = name;
-    }
-    
-    public void setEmail(String email){
-        this.email = email;
-    }
-    
-    public void setDepartment(String department){
-        this.department = department;
-    }
-    
-    public void setID(int ID){
-        this.ID = ID;
-    }
-    
-    public void setTempPassword(String tempPassword){
-        this.tempPassword = tempPassword;
-    }
-    
-    public void setRank(int rank){
-        this.rank = rank;
-    }
-    
-    public void setAssignment(String assignment){
-        this.assignment = assignment;
-    }
-    
-    public String getName(){
-        return name;
-    }
-    
-    public String getEmail(){
-        return email;
-    }
-    
-    public String getDepartment(){
-        return department;
-    }
-    
-    public int getID(){
-        return ID;
-    }
-    
-    public String getTempPassword(){
-        return tempPassword;
-    }
-    
-    public int getRank(){
-        return rank;
-    }
-    
-    public String getAssignment(){
-        return assignment;
-    }
-}
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -221,6 +74,25 @@ public class AdvisorAccount {
             }
         }
         else{
+            result = false;
+        }
+        
+        return result;
+    }
+    
+    public boolean initialize(String name, String department, int ID, String assignment){
+        boolean result = true;
+        
+        if(name != null && !name.isEmpty() && 
+                department != null && !department.isEmpty())
+        {
+            this.name = name;
+            this.department = department;
+            this.ID = ID;
+            this.assignment = assignment;
+        }
+        else
+        {
             result = false;
         }
         
